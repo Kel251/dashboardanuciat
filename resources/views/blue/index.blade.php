756cec8bac783dd5d2ab69555f204039f5715e89
Ezequiel <ezequiel@laptophp.local>
2017-12-20 20:59:18 -0600

    prueba

--- resources/views/blue/index.blade.php
+++ resources/views/blue/index.blade.php
@@ -1,21 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">  
-    <title>Perfil</title>
-    <meta name="description" content="">
-    <meta name="author" content="templatemo">
-    <!-- 
-    Visual Admin Template
-    http://www.templatemo.com/preview/templatemo_455_visual_admin
-    -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,700' rel='stylesheet' type='text/css'>
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/templatemo-style.css" rel="stylesheet">
-    
+    asdasdasdasdd
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -24,7 +10,7 @@
     <![endif]-->
 
   </head>
-  <body>  
+  <body>
     <!-- Left column -->
     <div class="templatemo-flex-row">
       <div class="templatemo-sidebar">
@@ -32,20 +18,20 @@
           <h1>Gerardo Moreno Torales</h1>
         </header>
         <div class="profile-photo-container">
-          <img src="images/formal.jpg" alt="Profile Photo" class="img-responsive">  
+          <img src="images/formal.jpg" alt="Profile Photo" class="img-responsive">
           <div class="profile-photo-overlay"></div>
-        </div>      
+        </div>
         <!-- Search box -->
         <form class="templatemo-search-form" role="search">
           <div class="input-group">
               <button type="submit" class="fa fa-search"></button>
-              <input type="text" class="form-control" placeholder="Buscar" name="srch-term" id="srch-term">           
+              <input type="text" class="form-control" placeholder="Buscar" name="srch-term" id="srch-term">
           </div>
         </form>
         <div class="mobile-menu-icon">
             <i class="fa fa-bars"></i>
         </div>
-        <nav class="templatemo-left-nav">          
+        <nav class="templatemo-left-nav">
           <ul>
             <li><a href="#" class="active"><i class="fa fa-home fa-fw"></i>Información Personal</a></li>
             <li><a href="data-visualization.html"><i class="fa fa-bar-chart fa-fw"></i>Estadisticas</a></li>
@@ -53,10 +39,10 @@
             <li><a href="manage-users.html"><i class="fa fa-users fa-fw"></i>Contactos</a></li>
             <li><a href="preferences.html"><i class="fa fa-sliders fa-fw"></i>Configuración</a></li>
             <li><a href="login.html"><i class="fa fa-eject fa-fw"></i>Cerrar sesion</a></li>
-          </ul>  
+          </ul>
         </nav>
       </div>
-      <!-- Main content --> 
+      <!-- Main content -->
       <div class="templatemo-content col-1 light-gray-bg">
 
         <div class="templatemo-content-container">
@@ -64,12 +50,12 @@
             <div class="templatemo-content-widget white-bg col-2">
               <i class="fa fa-times"></i>
               <h2 class="templatemo-inline-block">Desarrollador en Java</h2><hr>
-			  
-              <p>UNIVERSIDAD TECNOLOGICA DEL VALLE DE TOLUCA</p>              
-              <p>Toluca, Estado de México</p>              
-              <p>23 años </p>              
-              <p>Le interesa empresas desarrolladoras de software en JAVA</p>              
-              <p>3 años como desarrollador de software en GIOTIC</p>              
+
+              <p>UNIVERSIDAD TECNOLOGICA DEL VALLE DE TOLUCAs pruebas para github</p>
+              <p>Toluca, Estado de México</p>
+              <p>23 años </p>
+              <p>Le interesa empresas desarrolladoras de software en JAVA</p>
+              <p>3 años como desarrollador de software en GIOTIC</p>
 
 			  </div>
             <div class="templatemo-content-widget white-bg col-1 text-center">
@@ -86,9 +72,9 @@
             </div>
           </div>
           <div class="templatemo-flex-row flex-content-row">
-            <div class="col-1">              
+            <div class="col-1">
               <div class="templatemo-content-widget orange-bg">
-                <i class="fa fa-times"></i>                
+                <i class="fa fa-times"></i>
                 <div class="media">
                   <div class="media-left">
                     <a href="#">
@@ -97,11 +83,11 @@
                   </div>
                   <div class="media-body">
                     <h2 class="media-heading text-uppercase">Notas</h2>
-                    <p>Phasellus dapibus nulla quis risus auctor, non placerat augue consectetur.</p>  
-                  </div>        
-                </div>                
-              </div>            
-           
+                    <p>Phasellus dapibus nulla quis risus auctor, non placerat augue consectetur.</p>
+                  </div>
+                </div>
+              </div>
+
             </div>
             <div class="col-1">
               <div class="panel panel-default templatemo-content-widget white-bg no-padding templatemo-overflow-hidden">
@@ -131,12 +117,12 @@
                         <td>3.</td>
                         <td>Tester</td>
                         <td>Tecnogyes</td>
-                      </tr>                    
+                      </tr>
                     </tbody>
-                  </table>    
-                </div>                          
+                  </table>
+                </div>
               </div>
-            </div>           
+            </div>
           </div> <!-- Second row ends -->
           <div class="templatemo-flex-row flex-content-row templatemo-overflow-hidden"> <!-- overflow hidden for iPad mini landscape view-->
             <div class="col-1 templatemo-overflow-hidden">
@@ -150,32 +136,32 @@
                   <div class="col-1 col-lg-6 col-md-12">
                     <h2 class="text-center">Posibles Empresas<span class="badge">revisar</span></h2>
                     <div id="bar_chart_div" class="templatemo-chart"></div> <!-- Bar chart div -->
-                  </div>  
-                </div>                
+                  </div>
+                </div>
               </div>
             </div>
           </div>
           <footer class="text-right">
-            <p>Copyright &copy; 2084 Company Name 
+            <p>Copyright &copy; 2084 Company Name
             | Designed by <a href="http://www.templatemo.com" target="_parent">templatemo</a></p>
-          </footer>         
+          </footer>
         </div>
       </div>
     </div>
-    
+
     <!-- JS -->
     <script src="js/jquery-1.11.2.min.js"></script>      <!-- jQuery -->
     <script src="js/jquery-migrate-1.2.1.min.js"></script> <!--  jQuery Migrate Plugin -->
     <script src="https://www.google.com/jsapi"></script> <!-- Google Chart -->
     <script>
-      /* Google Chart 
+      /* Google Chart
       -------------------------------------------------------------------*/
       // Load the Visualization API and the piechart package.
       google.load('visualization', '1.0', {'packages':['corechart']});
 
       // Set a callback to run when the Google Visualization API is loaded.
-      google.setOnLoadCallback(drawChart); 
-      
+      google.setOnLoadCallback(drawChart);
+
       // Callback that creates and populates a data table,
       // instantiates the pie chart, passes in the data and
       // draws it.
@@ -215,14 +201,14 @@
             {
               this.location.reload(false); /* false to get page from cache */
             }, 200);
-          });      
+          });
         } else {
           $(window).resize(function(){
             drawChart();
-          });  
-        }   
+          });
+        }
       });
-      
+
     </script>
     <script type="text/javascript" src="js/templatemo-script.js"></script>      <!-- Templatemo Script -->
 
